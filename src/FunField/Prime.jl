e64544744762755a39984972c8a730c2ffaf99ce
Berenike Dieterle <94169337+BD-00@users.noreply.github.com>
2025-02-13 01:50:11 +0100

    collect more places - FactorBase for `Hecke.GenOrdIdl` - start collecting irreducible polys over Fq up to certain degree - places for higher degrees

--- src/FunField/Prime.jl
+++ src/FunField/Prime.jl
@@ -1,28 +1,77 @@
 function rational_primes(F::AbstractAlgebra.Generic.FunctionField{FqFieldElem})
- kt = F.base_ring #Fq(t)
- t = gen(kt)
- k = kt.base_ring #Fq
+ kt = base_ring(F) #Fq(t)
+ t = numerator(gen(kt))
+ k = base_ring(kt) #Fq
 
- I = [t+c for c in k] #irreducible polynomials of deg 1 over Fq
+ I = [t+c for c in k] #irreducible polynomials of deg 1 over Fq (warning: in Fq(t), so no test for irred. possible)
  fin_ord = finite_maximal_order(F)
  fin_places = []
  for p in I
   p_dec = prime_decomposition(fin_ord, numerator(p))
-  for (p,e) in p_dec
-   isone(degree(p)) && push!(fin_places, p_dec[1])
+  for (q,e) in p_dec
+   isone(degree(q)) && push!(fin_places, q)
   end
  end
  inf_ord = infinite_maximal_order(F)
  inf_places = []
  t_inv = gen(base_ring(inf_ord))
  inf_dec = prime_decomposition(inf_ord, t_inv)
- for (p,e) in inf_dec
-  isone(degree(p))&&push!(inf_places, p)
+ for (q,e) in inf_dec
+  isone(degree(q))&&push!(inf_places, q)
  end
- return fin_places, inf_places
+ return FactorBase(fin_places), FactorBase(inf_places)
 end
 
 
+function primes(F::AbstractAlgebra.Generic.FunctionField{FqFieldElem}, d::Int)
+ kt = base_ring(F) #Fq(t)
+ t = numerator(gen(kt))
+ k = base_ring(kt) #Fq
+
+ I = irred_polys(parent(t), d) #irreducible polynomials of deg 1 over Fq
+
+ fin_ord = finite_maximal_order(F)
+ fin_places = []
+ for p in I
+  deg_p = degree(p)
+  p_dec = prime_decomposition(fin_ord, p)
+  for (q,e) in p_dec
+   f = degree(q)
+   deg_q = f*deg_p
+   deg_q <= d && push!(fin_places, q) #TODO: sort by degree e.g. using Dict
+  end
+ end
+ inf_ord = infinite_maximal_order(F)
+ inf_places = []
+ t_inv = gen(base_ring(inf_ord))
+ inf_dec = prime_decomposition(inf_ord, t_inv)
+ for (q,e) in inf_dec
+  degree(q) <= d && push!(inf_places, q) #deg of inf place = 1, so f<=d enough?
+ end
+ return FactorBase(unique(fin_places)), FactorBase(unique(inf_places))
+end
+
+function gcd(a::T, b::T) where T<:Hecke.GenOrdIdl
+ return a+b
+end
+
+
+function irred_polys(R::FqPolyRing, d::Int)
+ t = gen(R)
+ Fq = base_ring(R)
+ q = length(Fq)
+ Fq_elems = collect(Fq)
+ I = [t+c for c in Fq]
+ for i = 2:d
+  for j = 0:q^d-1
+   idces = digits(j, base=q, pad=d)
+   coeffs = vcat([Fq(1)],[Fq_elems[idx+1] for idx in idces])
+   g = R(coeffs)
+   is_irreducible(g) && push!(I, g)
+  end
+ end
+ return I
+end
 
 
 ################################################################################
